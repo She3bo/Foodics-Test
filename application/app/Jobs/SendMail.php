161e9b5039b5840c4449cddcb74243fe91c1edce
she3bo <ahmedshaban33ah@gmail.com>
2023-11-07 00:31:53 +0200

    add SendEmail job

---
+++ application/app/Jobs/SendMail.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\StockAlertEmail;
+use App\Models\Product;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class SendMail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private Product $product;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(Product $product)
+    {
+        $this->product = $product;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Mail::to(config('mail.stoke_mail'))->send(new StockAlertEmail($this->product));
+    }
+}
