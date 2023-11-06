2dd6584fab9108c9264bf9b61e37ad9a73d8b34d
she3bo <ahmedshaban33ah@gmail.com>
2023-11-07 00:31:12 +0200

    add Stock Alert Email view

---
+++ application/resources/views/emails/stockAlert.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1> Hello,  </h1>
+
+<p> Stock Alert </p>
+<p>{{$product->name}} stock has less than 50% of ingredient</p>
+</body>
+</html>
